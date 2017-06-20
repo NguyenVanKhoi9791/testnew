6a461ab59cad92481e0064675e640bb0473e24d6
Van Soc Nguyen <vansocnguyen99@gmail.com>
2017-06-20 14:33:04 +0700

    first sumary
    sdsdsd

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Shane Nguyen on 6/20/17.
+//  Copyright © 2017 vansocnguyen99@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Shane Nguyen on 6/20/17.
+//  Copyright © 2017 vansocnguyen99@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
